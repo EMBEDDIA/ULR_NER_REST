f3f7e9ad708772bf114dcc67eac15dec515a7551
creat89 <adrian89_@hotmail.com>
2020-04-23 10:20:15 +0200

    Add and update license

--- services/celery-queue/celery_tasks.py
+++ services/celery-queue/celery_tasks.py
@@ -1,12 +1,28 @@
-import sys
-from celery import Celery, states
+# Embeddia Project - Named Entity Recognition
+# Copyright © 2020 Luis Adrián Cabrera Diego - La Rochelle Université
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+# to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+from celery import Celery, states
 from NER import getTags, generateDictOutput
 from NER_Manager import NERManager
 from celeryApiUtils.processInputText import readText
 
 celery_app = Celery("NER_API", broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
 NER_Manager = NERManager()
+
 
 @celery_app.task()
 def supportedLanguages():
