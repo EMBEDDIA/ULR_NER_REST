03600b58b3fc3119c64a33c6393e0ce3117e532f
creat89 <adrian89_@hotmail.com>
2021-03-23 10:12:11 +0100

    Project cleaning

--- services/ner-service/entrypoint.sh
+++ services/ner-service/entrypoint.sh
@@ -2,6 +2,6 @@
 
 #Using entrypoint seems to do not be keeping the environment set in the dockerfile, thus we need to activate it manually
 . /opt/conda/etc/profile.d/conda.sh
-conda activate ner_service	
+conda activate ner_service
 
 python NERService.py
